ce20679ce9756ac69255e663807f6fc5d877304d
GuQiuyi <1121847562@qq.com>
2026-01-19 20:47:16 +0800

    update repo_id

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -988,7 +988,7 @@ _CONFIGS = [
     #
     # Fine-tuning Franka configs.
     #
-TrainConfig(
+    TrainConfig(
         name="pi05_franka_finetune",
         
         # 使用 Pi0.5 配置
@@ -999,7 +999,7 @@ TrainConfig(
         
         data=LeRobotFrankaDataConfig(
             # 你的数据集路径 (对应 LEROBOT_HOME 下的 local/franka_place_0107)
-            repo_id="local/franka_pick_place_0115", 
+            repo_id="local/franka_pick_place_0118", 
             
             # 统计量 (Norm Stats) 路径
             # 理想情况下你应该先运行 compute_norm_stats 脚本计算自己的统计量
